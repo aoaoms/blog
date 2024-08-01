9946a8e7ecf9814ac4254916ff76ac9c6c35e745
aoaoms <aoaoms@outlook.com>
2024-08-01 23:37:30 +0800

    clear file, change index page

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,12 +3,19 @@ import Typed from 'typed.js'
 
 onMounted(() => {
     let options = {
-        strings: ['Hi, 欢迎来到我的个人网站. 2024/02/20 改版. 更多内容待更新...'],
+        strings: [`Hi, 每天都要快乐哦~ (●'◡'●)`],
         typeSpeed: 100,
         loop: false
     }
 
     let typed = new Typed('#typed', options)
+
+    setTimeout(() => {
+        // a标签跳转到blog页面
+        let a = document.createElement('a')
+        a.href = '/blog'
+        a.click()
+    }, 4500)
 })
 </script>
 
@@ -20,11 +27,10 @@ onMounted(() => {
         <div class="d-flex flex-column justify-content-center align-items-center col-md-6 col-sm-12 col-12"  style="height: 75vh;">
             <!-- typed js animate -->
             <div class="fs-5 position-relative">
-                <span class="position-absolute top-100 start-0 translate-right badge rounded-pill bg-light text-bg-light mb-5"
-                style="font-size: 12px;">
+                <span class="position-absolute top-100 start-0 translate-right badge rounded-pill bg-light text-bg-light mb-5">
                     By typed.js
                 </span>
-                <span id="typed"></span>
+                <span id="typed" class="display-6"></span>
             </div>
 
         </div>
